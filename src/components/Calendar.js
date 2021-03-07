43a752de6a10bd5352563d6fdc95deeaf3530523
ShainaBagri <shainabagri@gmail.com>
2021-03-06 23:31:31 -0800

    Deleted some unnecessary files

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,9 @@
 import React from "react";
 import styles from "../assets/Styles.module.css";
-import "date-fns";
 import Grid from "@material-ui/core/Grid";
 import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
-  KeyboardTimePicker,
   KeyboardDatePicker
 } from "@material-ui/pickers";
 
